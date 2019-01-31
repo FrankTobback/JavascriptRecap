d6aaa5e121b79c55cd8472eed34f607d924a2719
FrankTobback <franktobback@gmail.com>
2019-01-31 11:13:09 +0100

    intro recap

---
+++ Intro/main.js
@@ -0,0 +1,20 @@
+// Exercise 1.1
+let nummer = 5
+console.log(nummer);
+
+// Exercise 1.2
+let name = "Lambert";
+let firstName = "Gerard";
+let city = "Paris";
+console.log("Your name is " + firstName + name + " and you live in " + city);
+
+// Exercise 1.3
+/*function myFunction() {
+  var person = prompt("Please enter your name", " ");
+  if (person != null) {
+    document.getElementById("gegevens").innerHTML =
+    "Hello " + person;
+  }
+}*/
+
+// Exercise 1.4
